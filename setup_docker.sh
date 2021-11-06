54e6d058bf02ea28e42341e182dde938f17a1265
HuanXiang <32356815+ame1973@users.noreply.github.com>
2021-11-06 11:51:10 +0800

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -7,7 +7,8 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    apache2-utils
 
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
