c725dddf9cce28c8197461f9e2f723f2ae6e3552
HuanXiang <qq418610675@gmail.com>
2022-09-22 13:28:17 +0800

    update script

--- setup_docker.sh
+++ setup_docker.sh
@@ -10,15 +10,17 @@ sudo apt-get -y install \
     lsb-release \
     apache2-utils
 
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
-sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo service docker start
 
 sudo gpasswd -a $USER docker
 newgrp docker
